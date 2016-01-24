6f125ab3894698733148d39ae8e622e285cf9592
hyecookie <hyecookie@naver.com>
2016-01-25 00:43:49 +0900

    randomLog edit

--- chapter-2/practice-1.js
+++ chapter-2/practice-1.js
@@ -1,7 +1,8 @@
-var randomLoc = Math.floor(Math.random()*5);
-var location = randomLoc;
+var randomLoc = Math.floor(Math.random() * 5);
+var location1 = randomLoc;
 var location2 = location1 + 1;
 var location3 = location2 + 1;
+
 
 var guess;//사용자입력값
 var hits = 0;

5ca2e1c0455d650bfd2509dbc0db0de6e52af16b
hyecookie <hyecookie@naver.com>
2016-01-28 21:41:16 +0900

    edit date

--- chapter-3/date.js
+++ chapter-3/date.js
@@ -1,4 +1,4 @@
 var year = "20160128".substring (0, 4);
-var month = "20160128".substring (5, 6);
-var day = "20160128".substring (7, 8);
+var month = "20160128".substring (4, 6);
+var day = "20160128".substring (6, 8);
 alert ( year + "년" + month + "월" + day + "일" );

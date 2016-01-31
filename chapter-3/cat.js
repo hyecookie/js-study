053217f06c38f04b0452c3e0d11f4e9f8cf595cf
hyecookie <hyecookie@naver.com>
2016-01-31 15:40:14 +0900

    cat

--- chapter-3/cat.js
+++ chapter-3/cat.js
@@ -1,32 +1,13 @@
-user = prompt ("CAT을 입력하세요");
-c = user.charCodeAt(0)-2;
-a = user.charCodeAt(1)-2;
-t = user.charCodeAt(2)-2;
+userInput = prompt ("CAT을 입력하세요");
+replace = userInput.replace("CAT","CYT");
 
-String.fromCharCode(c);
-String.fromCharCode(a)
-String.fromCharCode(t);;
+if (userInput == "CAT") {
+  var c = replace.charCodeAt(0)-2;
+  console.log(c);
+  var a = replace.charCodeAt(1);
+  console.log(a);
+  var t = replace.charCodeAt(2)-2;
+  console.log(t);
+}
 
-
-
-
-
-
-//console.log(user);
-//console.log(c);
-//console.log(a);
-//console.log(t);
-
-var cat = ((c) + (a) + (t));
-
-console.log(cat);
-
-var res = cat.fromCharCode(0);
-
-
-alert(user.charAt(0));
-
-
-
-"CAT".CharCodeAt(0)-2
-String.fromCharCode("CAT".charCodeAt(0)-2)
+alert (String.fromCharCode(c) + String.fromCharCode(a) + String.fromCharCode(t));

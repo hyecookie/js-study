36e349913328f8c560b168c6fea1bcdb63c83654
hyecookie <hyecookie@naver.com>
2016-02-21 01:40:02 +0900

    수정

--- chapter3-4/rsp.js
+++ chapter3-4/rsp.js
@@ -1,18 +1,26 @@
 var userInput;
 var cpu = "가위";
-rst(userInput, cpu);
+rsp(userInput, cpu);
+var draw = false;
 
-function rst(userInput, cpu){
-  userInput = prompt ("가위, 바위, 보 중 하나를 입력해 주세요.");
-  if (userInput == cpu) {
-    alert("비겼습니다");
-  }
-  else if (userInput != cpu) {
-    if (userInput == "바위") {
-      alert("당신이 이겼습니다");
+function rsp(userInput, cpu){
+  while (!draw) {
+    userInput = prompt ("가위, 바위, 보 중 하나를 입력해 주세요.");
+    if (userInput === null) {
+    draw = true;
     }
-    if (userInput == "보") {
-      alert("당신이 졌습니다");
+
+    if (userInput == cpu) {
+      alert("비겼습니다");
+    }
+    if (userInput != cpu) {
+      if (userInput == "바위") {
+        alert("당신이 이겼습니다");
+      }
+      if (userInput == "보") {
+        alert("당신이 졌습니다");
+      }
+      break;
     }
   }
 }

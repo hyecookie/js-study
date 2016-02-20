7f1ce56e9ece07225a4df7eca87b669bac6d503e
hyecookie <hyecookie@naver.com>
2016-02-20 23:30:18 +0900

    가위바위

---
+++ chapter3-4/rockscissorspaper.js
@@ -0,0 +1,23 @@
+
+var cpu = ["가위","바위","보"];
+var random = Math.floor(Math.random() * cpu.length);
+var cpu = cpu[random];
+
+
+for (var i = 0; i < cpu.length; i = i + 1) {
+  var user = prompt("가위 바위 보 중 하나를 입력해 주세요.");
+  if(cpu[i]){
+    if((i = 0 && user === "가위") || (i = 1 && user === "바위") || (i = 2 && user === "보")){
+     alert("제가" + cpu + "를 냈으므로 비겼습니다");
+    }
+    if((i = 0 && user === "바위") || (i = 1 && user === "보") || (i = 2 && user === "가위")){
+     alert("제가" + cpu + "를 냈으므로 이겼습니다");
+     {break;}
+    }
+    if((i = 0 && user === "보") || (i = 1 && user === "가위") || (i = 2 && user === "바위")){
+     alert("제가" + cpu + "를 냈으므로 졌습니다");
+     {break;}
+   }
+  }
+
+}

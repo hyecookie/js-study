dbd33c1341cd5427873f97e6006c88b56fe835ad
hyecookie <hyecookie@naver.com>
2016-03-07 03:34:47 +0900

    edit

--- chapter-3-5/factorial2.js
+++ chapter-3-5/factorial2.js
@@ -1,10 +1,14 @@
 var userInput = prompt("숫자를 입력해 주세요.");
 factorial(userInput);
 
-function factorial(number){
 
+
+function factorial(number){
+  var result = "";
   if (number === 1) {
     return 1;
   }
   return number * factorial (number - 1);
 }
+
+alert(factorial(userInput));

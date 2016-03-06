3a972020dd678c527d82d2ed8aaa9bf4cb60df05
hyecookie <hyecookie@naver.com>
2016-03-07 05:20:00 +0900

    edot

--- chapter-3-5/mzp.js
+++ chapter-3-5/mzp.js
@@ -5,57 +5,24 @@ rsp(userInput, cpu1);
 var draw = false;
 
 function rsp(userInput, cpu1){
-  while (!draw) {
-    userInput = prompt ("가위, 바위, 보 중 하나를 입력해 주세요.");
-    replace1 = userInput.replace("가위","1");
-    replace2 = userInput.replace("바위","2");
-    replace3 = userInput.replace("보","3");
-
-    if (replace1 === null && replace2 === null && replace2 === null) {
-    }
-    if (replace1 == cpu1 || replace2 == cpu1 || replace3 == cpu1) {
-      alert("제가" + userInput + "를 냈으므로 비겼습니다. 다시 입력해 주세요.");
-      continue;
-
-    } else if ((replace1 != cpu1 && (cpu1 == 3)) || (replace2 != cpu1 && (cpu1 == 1)) || (replace3 != cpu1 && (cpu1 == 2))) {
-        userInput2 = prompt ("가위, 바위, 보 중 하나를 입력해 주세요.");
-        replace1 = userInput2.replace("가위","1");
-        replace2 = userInput2.replace("바위","2");
-        replace3 = userInput2.replace("보","3");
-
-        if (replace1 == cpu1 || replace2 == cpu1 || replace3 == cpu1) {
-          alert("제가" + userInput + "를 냈으므로 비겼습니다.");
-          break;
-
-        } else if ((replace1 != cpu1 && (cpu1 == 3)) || (replace2 != cpu1 && (cpu1 == 1)) || (replace3 != cpu1 && (cpu1 == 2))) {
-            alert("제가" + cpu1 + "를 냈습니다. 묵, 찌, 빠 중 하나를 입력해 주세요");
-            continue;
-          }
-          else if ((replace1 != cpu1 && (cpu1 == 2)) || (replace2 != cpu1 && (cpu1 == 3)) || (replace3 != cpu1 && (cpu1 == 1))) {
-            alert("제가" + cpu1 + "를 냈으므로 당신이 졌습니다" + " 1=가위, 2=바위, 3=보");
-            continue;
-          }
-      }
-      else if ((replace1 != cpu1 && (cpu1 == 2)) || (replace2 != cpu1 && (cpu1 == 3)) || (replace3 != cpu1 && (cpu1 == 1))) {
-        userInput3 = prompt ("가위, 바위, 보 중 하나를 입력해 주세요.");
-        replace1 = userInput3.replace("가위","1");
-        replace2 = userInput3.replace("바위","2");
-        replace3 = userInput3.replace("보","3");
-
-        if (replace1 == cpu1 || replace2 == cpu1 || replace3 == cpu1) {
-          alert("제가" + userInput + "를 냈으므로 비겼습니다.");
-          break;
-
-        } else if ((replace1 != cpu1 && (cpu1 == 3)) || (replace2 != cpu1 && (cpu1 == 1)) || (replace3 != cpu1 && (cpu1 == 2))) {
-            alert("제가" + cpu1 + "를 냈습니다. 묵, 찌, 빠 중 하나를 입력해 주세요");
-            continue;
-          }
-          else if ((replace1 != cpu1 && (cpu1 == 2)) || (replace2 != cpu1 && (cpu1 == 3)) || (replace3 != cpu1 && (cpu1 == 1))) {
-            alert("제가" + cpu1 + "를 냈으므로 당신이 졌습니다" + " 1=가위, 2=바위, 3=보");
-            continue;
-          }
-      }
-      break;
+  userInput = prompt ("묵, 찌, 빠 중 하나를 입력해 주세요.");
+  replace1 = userInput.replace("묵","1");
+  replace2 = userInput.replace("찌","2");
+  replace3 = userInput.replace("빠","3");
+  if (replace1 == cpu1 || replace2 == cpu1 || replace3 == cpu1) {
+      prompt("제가" + userInput + "를 냈으므로 비겼습니다. 다시 입력해 주세요.");
   }
+  else if((replace1 != cpu1) || (replace2 != cpu1) || (replace3 != cpu1)) {
+    userInput2 = prompt ("묵, 찌, 빠 중 하나를 다시 입력해 주세요.");
+    replace3 = userInput2.replace("가위","1");
+    replace4 = userInput2.replace("바위","2");
+    replace5 = userInput2.replace("보","3");
 
+    if ((replace3 != cpu1) || (replace4 != cpu1) || (replace5 != cpu1)) {
+        prompt("제가" + cpu1 + "를 냈습니다. 묵, 찌, 빠 중 하나를 입력해 주세요");
+
+    } else if (replace3 == cpu1 || replace4 == cpu1 || replace5 == cpu1) {
+       alert("제가" + userInput2 + "같은걸 냈습니다.");
+    }
+  }
 }

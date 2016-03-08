365626316ce489d9f7bb55f55efd90d6e8ab93ee
hyecookie <hyecookie@naver.com>
2016-03-09 01:35:02 +0900

    edit

--- chapter-3-5/mzp2.js
+++ chapter-3-5/mzp2.js
@@ -30,7 +30,7 @@ function mzp(){
 
    } else if (((userInput == "가위" ) && (cpu1 == "보")) || ((userInput == "바위") && (cpu1 == "가위")) || ((userInput == "보") && (cpu1=="바위"))) {
        alert("cpu가 " + cpu1 + "를(을) 내서 당신이 공격권을 가집니다." + " CPU: " + cpu1 + " User: " + userInput);
-       mzp2();
+       mzp3();
       }
   }
 
@@ -58,6 +58,34 @@ function mzp2(){
       mzp2();
     } else if (((userInput2 == "묵" ) && (cpu2 == "찌")) || ((userInput2 == "찌") && (cpu2 == "묵")) || ((userInput2 == "빠") && (cpu2 == "묵"))) {
       alert("cpu가 " + cpu2 + "를(을) 내서 당신이 공격권을 가집니다."+ " CPU: " + cpu2 + " User: " + userInput2);
+      mzp3();
+    }
+}
+
+function mzp3(){
+  var cpu3 = Math.floor(Math.random() * 3 + 1);
+  console.log(cpu3);
+  userInput3 = prompt ("묵, 찌, 빠 중 하나를 다시 입력해 주세요.");
+  if (userInput2 != "묵" && userInput3 != "찌" && userInput3 != "빠"){
+    alert("묵, 찌, 빠 만 입력 가능합니다.");
+    mzp3();
+    return;
+  }
+    if (cpu3 == 1) {
+        cpu3 = "묵";
+      } else if (cpu3 == 2) {
+        cpu3 = "찌";
+      } else if (cpu3 == 3){
+        cpu3 = "빠";
+    }
+
+     if (userInput3 == cpu3) {
+      alert("cpu가 " + cpu3 + "를(을) 냈으므로 당신이 이겼습니다." + " CPU: " + cpu3 + " User: " + userInput3);
+    } else if (((userInput3 == "묵" ) && (cpu3 == "빠")) || ((userInput3 == "찌") && (cpu3 == "묵")) || ((userInput3 == "빠") && (cpu3 == "찌"))) {
+      alert("cpu가 " + cpu3 + "를(을) 내서 공격권을 가집니다."+ " CPU: " + cpu3 + " User: " + userInput3);
       mzp2();
+    } else if (((userInput3 == "묵" ) && (cpu3 == "찌")) || ((userInput3 == "찌") && (cpu3 == "묵")) || ((userInput3 == "빠") && (cpu3 == "묵"))) {
+      alert("cpu가 " + cpu3 + "를(을) 내서 당신이 공격권을 가집니다."+ " CPU: " + cpu3 + " User: " + userInput3);
+      mzp3();
     }
 }

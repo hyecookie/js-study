118bb97e9fb9c5d3c77ce943701af26c641c4d2b
hyecookie <hyecookie@naver.com>
2016-04-26 18:20:54 +0900

    clock practice

--- chapter-5/practice2.js
+++ chapter-5/practice2.js
@@ -1,11 +1,17 @@
 var clock = {
   alam : function() {
     var alam = prompt('알람시간을 입력해 주세요 24시 기준');
+    var hours = Math.floor(Math.random() * this.hours + 1);
     if (alam == this.hours) {
-      alert('알람입니다');
+      alert('알람입니다. 때르릉');
+    } else if (alam != this.horus) {
+      alert('...');
     }
+    return;
   },
   hours : 24,
   minutes : 60,
   seconds : 86400,
 };
+
+clock.alam ();
